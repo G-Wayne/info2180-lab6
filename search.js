b5df0e03c4234462d21f9458871423bfbc26a135
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-10-29 15:45:03 -0400

    This is Exercise 3

--- search.js
+++ search.js
@@ -1,23 +1,37 @@
 window.onload = function(){
+
   var button = document.getElementById("button");
   button.addEventListener("click",search);
 
   var httpRequest = new XMLHttpRequest();
-  var url = "https://info2180-lab6-glawton.c9users.io/request.php?q=definition";
+  var sourceUrl = "https://info2180-lab6-glawton.c9users.io/request.php?q=";
+
 
   function search(){
+    var answer = document.getElementById('result');
+    var searchBy = document.getElementById("check").value.toLowerCase();
+    if(searchBy.length === 0){
+      answer.innerHTML = "No search criteria entered";
+    }else{
+      var url = sourceUrl + searchBy;
+      /*alert(url);*/
+      httpRequest.open("GET",url, true);
+      httpRequest.send();
+
     httpRequest.onreadystatechange = function(){
       if (httpRequest.readyState===XMLHttpRequest.DONE){
         if(httpRequest.status===200){
           var response = httpRequest.responseText;
-          alert(response);
-      }else{
-          alert("There was a problem with the request.");
+          /*alert(response);*/
+          answer.innerHTML = response;
+        }else{
+          answer.innerHTML = "There was a problem with the request.Invalid search criteria";
+        }
+
       }
     }
 
   }
-    httpRequest.open("GET",url, true);
-    httpRequest.send();
+
 }
 }
