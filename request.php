2365f6ac6cb58f9d28fff41160db7dcf59a49936
G-Wayne <32604878+G-Wayne@users.noreply.github.com>
2017-11-03 02:00:31 -0400

    This is Exercise 4

--- request.php
+++ request.php
@@ -4,6 +4,8 @@
 // respond with a value
 
 $query = $_GET['q'];
+$all = $_GET['all'];
+
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -13,6 +15,58 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name ="definition" author ="Jason">
+        A statement of the exact meaning of a word, especially in a dictionary.
+        </definition>
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+
+
+        <definition name ="bar" author ="Travis">
+        A place that sells alcholic beverages.</definition>
+
+
+
+        <definition name ="ajax" author ="Leeroy">
+        Technique which involves the use of javascript and xml (or JSON).
+        </definition>
+
+
+
+        <definition name ="html" author ="Kemesha">
+        The standard markup language for creating web pages and web applications.
+        </definition>
+
+
+
+        <definition name ="css" author ="Markland">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+        </definition>
+
+
+
+        <definition name ="javascript" author ="Aaron">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+        </definition>
+
+
+
+        <definition name ="php" author ="Leerose">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+        </definition>
+</entries>';
+
+if ($query === "" && $all === "true"){
+    header('Content-Type: application/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+
+}
+else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
+
+
+?>
--- search.js
+++ search.js
@@ -1,20 +1,28 @@
 window.onload = function(){
 
+  //For search Button
   var button = document.getElementById("button");
   button.addEventListener("click",search);
 
+  //For Get All Defintions Button
+  var allDefinitions = document.getElementById("definitions");
+  allDefinitions.addEventListener("click",searchAll);
+
+  //For search
   var httpRequest = new XMLHttpRequest();
+  //For Get All Defintions
+  var httpRequest2 = new XMLHttpRequest();
+  var answer = document.getElementById('result');
   var sourceUrl = "https://info2180-lab6-glawton.c9users.io/request.php?q=";
 
-
+  //For search button
   function search(){
-    var answer = document.getElementById('result');
     var searchBy = document.getElementById("check").value.toLowerCase();
     if(searchBy.length === 0){
       answer.innerHTML = "No search criteria entered";
     }else{
       var url = sourceUrl + searchBy;
-      /*alert(url);*/
+      //alert(url);
       httpRequest.open("GET",url, true);
       httpRequest.send();
 
@@ -22,16 +30,76 @@ window.onload = function(){
       if (httpRequest.readyState===XMLHttpRequest.DONE){
         if(httpRequest.status===200){
           var response = httpRequest.responseText;
-          /*alert(response);*/
+          //alert(response);
           answer.innerHTML = response;
         }else{
           answer.innerHTML = "There was a problem with the request.Invalid search criteria";
         }
+
 
       }
     }
 
   }
 
+}
+
+var parser = new window.DOMParser();
+
+//For all definitions search button
+function searchAll() {
+
+    var allUrl = "https://info2180-lab6-glawton.c9users.io/request.php?q=&all=true";
+    httpRequest2.open("GET",allUrl, true);
+    httpRequest2.send();
+
+      httpRequest2.onreadystatechange = function(){
+        if (httpRequest2.readyState===XMLHttpRequest.DONE){
+          if(httpRequest2.status===200){
+            var xmlDoc = parser.parseFromString(this.responseText, "application/xml");
+            console.log(xmlDoc);
+
+    			var definitions = xmlDoc.getElementsByTagName("definition");
+    			//create ordered list
+    			var list = document.createElement("ol");
+
+    			//iterate through definition tags and add to list.
+    			for (var tag =0; tag<definitions.length; tag++){
+
+    			  //create tags
+    			  var heading = document.createElement("h3");
+    			  var details = document.createElement("p");
+    			  var author = document.createElement("p");
+    			  var item = document.createElement("li");
+
+    			  //create text
+    			  var text = document.createTextNode(definitions[tag].childNodes[0].nodeValue);
+    			  var headingText = document.createTextNode(definitions[tag].attributes[0].value.toUpperCase());
+    			  var authorText = document.createTextNode('-' + definitions[tag].attributes[1].value);
+
+    			  //Add text to tags
+    			  heading.appendChild(headingText);
+    			  details.appendChild(text);
+    			  author.appendChild(authorText);
+
+    			  //Add tags to list items
+    			  item.appendChild(heading);
+    			  item.appendChild(details);
+    			  item.appendChild(author);
+
+            //All all tags created to the list
+    			  list.appendChild(item);
+
+    			}
+
+    			answer.appendChild(list);
+
+
+        }
+
+
+      }
+
+}
 }
 }
